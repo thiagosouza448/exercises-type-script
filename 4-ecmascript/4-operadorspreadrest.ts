bfb924616d44984651b951a9952a2f7b97c467f4
Thiago Souza <31426826+thiagosouza448@users.noreply.github.com>
2024-02-05 19:01:25 -0300

    ecma

---
+++ 4-ecmascript/4-operadorspreadrest.ts
@@ -0,0 +1,33 @@
+// Rest & Spread
+const numbers = [1, 10, 99, -5, 200, 1034]
+console.log(Math.max(...numbers))
+
+const turmaA: string[] = ['João', 'Maria', 'Fernanda']
+const turmaB: string[] = 
+    ['Fernando', ...turmaA, 'Miguel', 'Lorena']
+console.log(turmaB)
+
+function retornarArray(...args: number[]): number[] {
+    return args
+}
+
+const numeros = retornarArray(1, 2, 4, 5, 6, 345, 623)
+console.log(numeros)
+console.log(retornarArray(...numbers))
+
+
+// Rest & Spread (Tupla)
+const tupla: [number, string, boolean] = [1, 'abc', false]
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+    console.log(`1) ${a} ${b} ${c}`)
+}
+
+tuplaParam1(...tupla)
+
+function tuplaParam2(...params: [number, string, boolean]) {
+    // console.log(Array.isArray(params))
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`)
+}
+
+tuplaParam2(...tupla)
